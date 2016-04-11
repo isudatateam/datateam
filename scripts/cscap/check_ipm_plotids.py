955192a82c121c785fae148f383d2adef23e7f21
akrherz <akrherz@iastate.edu>
2016-04-11 08:53:22 -0500

    check USB IPM plotids

--- scripts/cscap/check_ipm_plotids.py
+++ scripts/cscap/check_ipm_plotids.py
@@ -5,24 +5,40 @@ import pandas as pd
 
 pgconn = psycopg2.connect(database='sustainablecorn')
 
-plotdf = read_sql("""SELECT upper(uniqueid) as uniqueid, plotid from plotids""",
-                  pgconn, index_col=None)
+plotdf = read_sql("""
+    SELECT upper(uniqueid) as uniqueid, plotid from plotids
+    """, pgconn, index_col=None)
+
+drive = utils.get_driveclient(utils.get_config(), 'cscap')
 
 spr_client = utils.get_spreadsheet_client(utils.get_config())
 
-spreadsheet = utils.Spreadsheet(
-        spr_client, '1WxB9pWvTB-_8cErQgXrybX7rklu5qXL8XyWtEf7yDVs')
-rows = []
-for worksheet in spreadsheet.worksheets:
-    lf = spreadsheet.worksheets[worksheet].get_list_feed()
-    for entry in lf.entry:
-        d = entry.to_dict()
-        rows.append(dict(uniqueid=d['site'], plotid=d['plotid']))
+res = drive.files().list(
+    q=("'0B4fyEPcRW7IscDcweEwxUFV3YkU' in parents and "
+       "title contains 'USB'")).execute()
 
-plotipm = pd.DataFrame(rows)
-for i, row in plotipm.iterrows():
-    df2 = plotdf[(plotdf['uniqueid'] == row['uniqueid']) &
-                 (plotdf['plotid'] == row['plotid'])]
-    if len(df2.index) == 0:
-        print("Missing uniqueid: |%s| plotid: |%s|" % (row['uniqueid'],
-                                                       row['plotid']))
+U = {}
+
+for item in res['items']:
+    if item['mimeType'] != 'application/vnd.google-apps.spreadsheet':
+        continue
+    uniqueid = item['title'].split("-")[1].strip()
+    spreadsheet = utils.Spreadsheet(spr_client, item['id'])
+    rows = []
+    for worksheet in spreadsheet.worksheets:
+        lf = spreadsheet.worksheets[worksheet].get_list_feed()
+        for entry in lf.entry:
+            d = entry.to_dict()
+            rows.append(dict(uniqueid=uniqueid, plotid=d['plotid']))
+
+    plotipm = pd.DataFrame(rows)
+    for i, row in plotipm.iterrows():
+        df2 = plotdf[(plotdf['uniqueid'] == row['uniqueid']) &
+                     (plotdf['plotid'] == row['plotid'])]
+        if len(df2.index) == 0:
+            key = "%s_%s" % (row['uniqueid'], row['plotid'])
+            if key in U:
+                continue
+            U[key] = True
+            print("Missing uniqueid: |%s| plotid: |%s|" % (row['uniqueid'],
+                                                           row['plotid']))
