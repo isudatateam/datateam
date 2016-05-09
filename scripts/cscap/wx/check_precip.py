f16e35594c94d0e72e946a2804acca9338e48360
akrherz <akrherz@iastate.edu>
2016-05-09 09:48:39 -0500

    more wx precip dump fun

---
+++ scripts/cscap/wx/check_precip.py
@@ -0,0 +1,22 @@
+"""A crude check on CSCAP Precip Totals"""
+from pyiem.network import Table as NetworkTable
+import psycopg2
+from pandas.io.sql import read_sql
+
+nt = NetworkTable("CSCAP")
+
+pgconn = psycopg2.connect(database='coop', host='iemdb', user='nobody')
+
+done = []
+for sid in nt.sts.keys():
+    clid = nt.sts[sid]['climate_site']
+    if clid in done:
+        continue
+    done.append(clid)
+    print("------------------------- %s %s" % (sid, clid))
+    df = read_sql("""
+    SELECT year, sum(precip) from alldata_"""+clid[:2]+"""
+    WHERE station = %s
+    GROUP by year""", pgconn, params=(clid,), index_col=None)
+    for i, row in df[df['sum'] < 10].iterrows():
+        print sid, clid, row['year'], row['sum']
---
+++ scripts/cscap/wx/rye_fun3.py
@@ -0,0 +1,23 @@
+import psycopg2
+import datetime
+from pyiem.network import Table as NetworkTable
+nt = NetworkTable("CSCAP")
+COOP = psycopg2.connect(database='coop', host='iemdb', user='nobody')
+ccursor = COOP.cursor()
+
+SITES = ['MASON', 'KELLOGG', 'GILMORE', 'ISUAG', 'WOOSTER.COV',
+         'SEPAC', 'FREEMAN', 'BRADFORD.C',
+         'BRADFORD.B1', 'BRADFORD.B2']
+
+print("uniqueid,date,high[F],low[F],precip[in]")
+for uniqueid in SITES:
+    for year in range(2011, 2016):
+        plantdate = datetime.date(year-1, 10, 15)
+        terminatedate = datetime.date(year, 4, 15)
+        clsite = nt.sts[uniqueid]['climate_site']
+        ccursor.execute("""SELECT day, high, low, precip from
+        alldata_""" + clsite[:2] + """ WHERE station = %s and day >= %s
+        and day <= %s ORDER by day ASC""", (clsite, plantdate, terminatedate))
+        for row in ccursor:
+            print("%s,%s,%s,%s,%s" % (uniqueid, row[0], row[1], row[2],
+                                      row[3]))
