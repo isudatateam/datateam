6c43964ece4aecc82390b501a91064e5b36b2827
akrherz <akrherz@iastate.edu>
2016-05-17 14:45:48 -0500

    add more columns to plotid sheets

---
+++ database/upgrade/sustainablecorn/16.sql
@@ -0,0 +1,5 @@
+ALTER TABLE plotids add agro varchar(3);
+ALTER TABLE plotids add soil varchar(3);
+ALTER TABLE plotids add ghg varchar(3);
+ALTER TABLE plotids add ipm_cscap varchar(3);
+ALTER TABLE plotids add ipm_usb varchar(3);
