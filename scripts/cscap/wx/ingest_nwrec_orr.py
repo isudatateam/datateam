aff410566ed5fa3143a18698824712e9f7fc4f08
akrherz <akrherz@iastate.edu>
2016-11-03 11:37:07 -0500

    wx ingest work

---
+++ scripts/cscap/wx/ingest_nwrec_orr.py
@@ -0,0 +1,39 @@
+"""
+http://www.isws.illinois.edu/warm/datatype.asp
+"""
+import pandas as pd
+import datetime
+from pyiem.datatypes import speed
+import psycopg2
+pgconn = psycopg2.connect(database='sustainablecorn', host='iemdb')
+
+station = 'ORR'
+fn = 'ORRDAY.txt'
+
+df = pd.read_table(fn, skiprows=[1])
+df['sknt'] = speed(pd.to_numeric(df['avg_wind_speed'], errors='coerse'),
+                   'MPH').value('KT')
+df['high'] = pd.to_numeric(df['max_air_temp'], errors='coerse')
+df['low'] = pd.to_numeric(df['min_air_temp'], errors='coerse')
+df['pday'] = df['precip']
+df['srad'] = df['sol_rad']
+df['date'] = df[['year', 'month', 'day']].apply(lambda x:
+                                                datetime.date(x[0], x[1],
+                                                              x[2]),
+                                                axis=1)
+print("fn: %s valid: %s - %s" % (fn, df['date'].min(), df['date'].max()))
+cursor = pgconn.cursor()
+cursor.execute("""DELETE from weather_data_daily where station = %s
+    and valid >= %s and valid <= %s
+    """, (station, df['date'].min(), df['date'].max()))
+if cursor.rowcount > 0:
+    print("Deleted %s rows" % (cursor.rowcount, ))
+
+for i, row in df.iterrows():
+    cursor.execute("""INSERT into weather_data_daily
+    (station, valid, high, low, precip, sknt, srad_mj) VALUES (%s,
+    %s, %s, %s, %s, %s, %s)
+    """, (station, row['date'], row['high'], row['low'],
+          row['pday'], row['sknt'], row['srad']))
+cursor.close()
+pgconn.commit()
---
+++ scripts/cscap/wx/ingest_sepac.py
@@ -0,0 +1,37 @@
+"""
+http://www.isws.illinois.edu/warm/datatype.asp
+"""
+import pandas as pd
+import datetime
+from pyiem.datatypes import speed
+import psycopg2
+pgconn = psycopg2.connect(database='sustainablecorn', host='iemdb')
+
+station = 'SEPAC'
+fn = 'sepac.xlsx'
+
+df = pd.read_excel(fn)
+print df.columns
+df['sknt'] = speed(pd.to_numeric(df['Wind Speed(mph)'], errors='coerse'),
+                   'MPH').value('KT')
+df['high'] = pd.to_numeric(df['Maximum Air Temperature(degF)'], errors='coerse')
+df['low'] = pd.to_numeric(df['Minimum Air Temperature(degF)'], errors='coerse')
+df['pday'] = df['Precipitation(inch)']
+df['srad'] = df['Solar Radiation(MJsqm)']
+
+print("fn: %s valid: %s - %s" % (fn, df['date'].min(), df['date'].max()))
+cursor = pgconn.cursor()
+cursor.execute("""DELETE from weather_data_daily where station = %s
+    and valid >= %s and valid <= %s
+    """, (station, df['date'].min(), df['date'].max()))
+if cursor.rowcount > 0:
+    print("Deleted %s rows" % (cursor.rowcount, ))
+
+for i, row in df.iterrows():
+    cursor.execute("""INSERT into weather_data_daily
+    (station, valid, high, low, precip, sknt, srad_mj) VALUES (%s,
+    %s, %s, %s, %s, %s, %s)
+    """, (station, row['date'], row['high'], row['low'],
+          row['pday'], row['sknt'], row['srad']))
+cursor.close()
+pgconn.commit()
