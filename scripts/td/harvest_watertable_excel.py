5cd359dd1968ab4602a87d33b4efe4c1b45ccc1e
akrherz <akrherz@iastate.edu>
2017-03-23 09:56:21 -0500

    [td] process TIDE_OLD watertable

--- scripts/td/harvest_watertable_excel.py
+++ scripts/td/harvest_watertable_excel.py
@@ -9,17 +9,16 @@ dfs = pd.read_excel(sys.argv[1], skiprows=[1], sheetname=None)
 uniqueid = sys.argv[2]
 
 for year in dfs.keys():
-    #if len(year) == 4:
-    #    print("skipping sheet: %s" % (year,))
-    #    continue
+    if len(year) == 4:
+        print("skipping sheet: %s" % (year,))
+        continue
     print("Processing sheet: %s" % (year,))
     df = dfs[year]
     if len(df.index) == 0:
         print("Empty Sheet, skipping")
         continue
-    df.columns = ['Date', 'b', '1', 'b', '2', 'b', '3', 'b', '4', 'b', '5', 'b', '6', 'b', '7', 'b', '8', 'b',
-                  '9', 'b', '10', 'b', '11', 'b', '12', 'b', '13', 'b', '14', 'b', '15', 'b', '16']
-    # df['Date'] = df['Date'] - datetime.timedelta(hours=1)
+    df.columns = ['Date', 'H2A-W1', 'H3A-W1', 'H4A-W1', 'H5A-W1']
+    df['Date'] = df['Date'] - datetime.timedelta(hours=1)
     # df['Date'] = pd.to_datetime(df['Date'].dt.strftime("%Y-%m-%d") + ' ' + df['Time'].astype(str))
     cursor.execute("""
         DELETE from watertable_data where uniqueid = %s and
@@ -29,15 +28,14 @@ for year in dfs.keys():
     if deleted > 0:
         print("Removed %s" % (deleted,))
     inserts = 0
-    for plotid in ['1', '2', '3', '4', '5', '6', '7', '8',
-                   '9', '10', '11', '12', '13', '14', '15', '16']:
+    for plotid in ['H2A-W1', 'H3A-W1', 'H4A-W1', 'H5A-W1']:
         for idx, row in df.iterrows():
             if (row['Date'] == ' ' or row['Date'] is None or
                     row.get(plotid) in ['dnc', 'nd']):
                 continue
             try:
                 val = row.get(plotid) * 10.
-            except:
+            except ValueError:
                 continue
             cursor.execute("""
             INSERT into watertable_data
