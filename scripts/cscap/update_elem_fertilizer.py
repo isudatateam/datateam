d7c2a58a10db6825fdb9f434960629dc4bf8c25d
akrherz <akrherz@iastate.edu>
2017-04-24 08:54:52 -0500

    correct Option B units assumption in elemental fert conversion
    refs #9

--- scripts/cscap/update_elem_fertilizer.py
+++ scripts/cscap/update_elem_fertilizer.py
@@ -1,60 +1,75 @@
 """Update the Elemental Fertiziler Columns in the backend datasheet..."""
+from __future__ import print_function
 import pyiem.cscap_utils as util
 
-sprclient = util.get_spreadsheet_client(util.get_config())
-
-spreadsheet = util.Spreadsheet(sprclient,
-                               '1CdPi6IEnO3T35t-OFQajKYzFLpu7hwOaWdL2FG1_kVA')
-spreadsheet.get_worksheets()
-sheet = spreadsheet.worksheets['Field Operations']
+KGHA_LBA = 1.12085
+BACKEND = '1CdPi6IEnO3T35t-OFQajKYzFLpu7hwOaWdL2FG1_kVA'
 
 
 def float2(val):
+    """type conversion"""
     if val is None:
         return 0
     return float(val)
 
 
-for entry in sheet.get_list_feed().entry:
-    d = entry.to_dict()
-    if d['operation'] != 'fertilizer_synthetic':
-        continue
-    if d['productrate'] is None or d['productrate'] == '-1.0':
-        # Option B, values are in kg ha-1
-        n = float2(d['nitrogen'])
-        p = float2(d['phosphate'])
-        k = float2(d['potash'])
-        s = float2(d['sulfur'])
-        z = float2(d['zinc'])
-        m = float2(d['magnesium'])
-        c = float2(d['calcium'])
-        f = float2(d['iron'])
-    else:
-        # Option A, straight percentages
-        prate = float2(d['productrate'])
-        n = prate * float2(d['nitrogen']) / 100.
-        p = 0
-        k = 0
-        if d['phosphorus'] is not None and float2(d['phosphorus']) > 0:
-            p = prate * float2(d['phosphorus']) / 100.
-        if d['phosphate'] is not None and float2(d['phosphate']) > 0:
-            p = prate * float2(d['phosphate']) / 100. * 0.437
-        if d['potassium'] is not None and float2(d['potassium']) > 0:
-            k = prate * float2(d['potassium']) / 100.
-        if d['potash'] is not None and float2(d['potash']) > 0:
-            k = prate * float(d['potash']) / 100. * 0.830
-        s = prate * float2(d['sulfur']) / 100.
-        z = prate * float2(d['zinc']) / 100.
-        m = prate * float2(d['magnesium']) / 100.
-        c = prate * float2(d['calcium']) / 100.
-        f = prate * float2(d['iron']) / 100.
+def main():
+    """Do Main Things"""
+    sprclient = util.get_spreadsheet_client(util.get_config())
 
-    entry.set_value('nitrogenelem', "%.2f" % (n, ))
-    entry.set_value('phosphoruselem', "%.2f" % (p, ))
-    entry.set_value('potassiumelem', "%.2f" % (k, ))
-    entry.set_value('sulfurelem', "%.2f" % (s, ))
-    entry.set_value('zincelem', "%.2f" % (z, ))
-    entry.set_value('magnesiumelem', "%.2f" % (m, ))
-    entry.set_value('calciumelem', "%.2f" % (c, ))
-    entry.set_value('ironelem', "%.2f" % (f, ))
-    sprclient.update(entry)
+    spreadsheet = util.Spreadsheet(sprclient, BACKEND)
+    spreadsheet.get_worksheets()
+    sheet = spreadsheet.worksheets['Field Operations']
+
+    for entry in sheet.get_list_feed().entry:
+        row = entry.to_dict()
+        if row['operation'] != 'fertilizer_synthetic':
+            continue
+        if (row['productrate'] is None or row['productrate'] == '-1.0' or
+                row['productrate'] == ''):
+            option = 'B'
+            # Option B, values are in lbs per acre
+            nitrogen = float2(row['nitrogen']) * KGHA_LBA
+            phosphoruse = float2(row['phosphate']) * KGHA_LBA
+            potassium = float2(row['potash']) * KGHA_LBA
+            sulfur = float2(row['sulfur']) * KGHA_LBA
+            zinc = float2(row['zinc']) * KGHA_LBA
+            magnesium = float2(row['magnesium']) * KGHA_LBA
+            calcium = float2(row['calcium']) * KGHA_LBA
+            iron = float2(row['iron']) * KGHA_LBA
+        else:
+            option = 'A'
+            # Option A, straight percentages
+            prate = float2(row['productrate'])
+            nitrogen = prate * float2(row['nitrogen']) / 100.
+            phosphoruse = 0.
+            potassium = 0.
+            if row['phosphorus'] is not None and float2(row['phosphorus']) > 0:
+                phosphoruse = prate * float2(row['phosphorus']) / 100.
+            if row['phosphate'] is not None and float2(row['phosphate']) > 0:
+                phosphoruse = prate * float2(row['phosphate']) / 100. * 0.437
+            if row['potassium'] is not None and float2(row['potassium']) > 0:
+                potassium = prate * float2(row['potassium']) / 100.
+            if row['potash'] is not None and float2(row['potash']) > 0:
+                potassium = prate * float(row['potash']) / 100. * 0.830
+            sulfur = prate * float2(row['sulfur']) / 100.
+            zinc = prate * float2(row['zinc']) / 100.
+            magnesium = prate * float2(row['magnesium']) / 100.
+            calcium = prate * float2(row['calcium']) / 100.
+            iron = prate * float2(row['iron']) / 100.
+
+        print(("Option: %s\n    nitrogen: Old: %s -> New: %s"
+               ) % (option, row['nitrogenelem'], nitrogen))
+        entry.set_value('nitrogenelem', "%.2f" % (nitrogen, ))
+        entry.set_value('phosphoruselem', "%.2f" % (phosphoruse, ))
+        entry.set_value('potassiumelem', "%.2f" % (potassium, ))
+        entry.set_value('sulfurelem', "%.2f" % (sulfur, ))
+        entry.set_value('zincelem', "%.2f" % (zinc, ))
+        entry.set_value('magnesiumelem', "%.2f" % (magnesium, ))
+        entry.set_value('calciumelem', "%.2f" % (calcium, ))
+        entry.set_value('ironelem', "%.2f" % (iron, ))
+        sprclient.update(entry)
+
+
+if __name__ == '__main__':
+    main()
