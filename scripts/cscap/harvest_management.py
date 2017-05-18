c713ddfb2b7be5d1625d9d850370a439d649b6c0
akrherz <akrherz@iastate.edu>
2017-05-18 12:49:56 -0500

    stop being cute with only updating management entries

--- scripts/cscap/harvest_management.py
+++ scripts/cscap/harvest_management.py
@@ -1,102 +1,67 @@
 """Harvest the data in the data management store!"""
+from __future__ import print_function
 import pyiem.cscap_utils as util
 import psycopg2
 
-config = util.get_config()
 
-pgconn = psycopg2.connect(database='sustainablecorn', user='mesonet',
-                          host=config['database']['host'])
-pcursor = pgconn.cursor()
+def main():
+    """Go Main"""
+    config = util.get_config()
 
-# Get me a client, stat
-spr_client = util.get_spreadsheet_client(config)
+    pgconn = psycopg2.connect(database='sustainablecorn', user='mesonet',
+                              host=config['database']['host'])
+    pcursor = pgconn.cursor()
 
-spread = util.Spreadsheet(spr_client, config['cscap']['manstore'])
+    # Get me a client, stat
+    spr_client = util.get_spreadsheet_client(config)
 
-translate = {'date': 'valid'}
+    spread = util.Spreadsheet(spr_client, config['cscap']['manstore'])
 
+    translate = {'date': 'valid'}
 
-TABS = ['Field Operations', 'Management', 'Pesticides']
-for sheetkey in TABS:
-    current = {}
-    found = {}
-    if sheetkey == TABS[0]:
-        pcursor.execute("""
-            SELECT valid, uniqueid, updated, operation, cropyear, oid
-            from operations
-        """)
-    elif sheetkey == 'Management':
-        pcursor.execute("""
-            SELECT '', uniqueid,    updated, '',       cropyear, oid
-            from management
-        """)
-    else:
-        pcursor.execute("""
-            SELECT valid, uniqueid, updated, crop, cropyear, oid
-            from pesticides
-        """)
-    for row in pcursor:
-        v = row[0]
-        if v is not None and not isinstance(v, str):
-            v = v.strftime("%-m/%-d/%Y")
-        key = "%s,%s,%s,%s,%s" % (v, row[1], row[2], row[3], row[4])
-        current[key] = row[5]
-    sheet = spread.worksheets[sheetkey]
-    table = sheetkey if sheetkey != TABS[0] else 'operations'
+    tabs = ['Field Operations', 'Management', 'Pesticides']
+    tablenames = ['operations', 'management', 'pesticides']
+    for sheetkey, table in zip(tabs, tablenames):
+        pcursor.execute("""DELETE from """ + table)
+        deleted = pcursor.rowcount
+        sheet = spread.worksheets[sheetkey]
 
-    added = 0
-    dups = 0
-    entries = 0
-    for rownum, entry in enumerate(sheet.get_list_feed().entry):
-        # Skip the first row of units
-        if rownum == 0:
-            continue
-        entries += 1
-        d = entry.to_dict()
-        # Units row has n/a as the date, so skip it
-        if d.get('date') in ['mm/dd/yyyy', '', None]:
-            continue
-        key = ('%s,%s,%s,%s,%s'
-               ) % (d.get('date', ''), d.get('uniqueid'), d.get('updated'),
-                    d.get('operation' if sheetkey == TABS[0] else 'crop',
-                          ''), d.get('cropyear'))
-        if key in current:
-            del(current[key])
-            found[key] = True
-            dups += 1
-            continue
-        if key in found:
-            _t = key.split(",")
-            print(("harvest_management DUP: %s on %s for %s[%s]"
-                   ) % (_t[3], _t[0], _t[1], _t[4]))
-            continue
-        cols = []
-        vals = []
-        for key in d.keys():
-            if key.startswith('gio'):
+        added = 0
+        for rownum, entry in enumerate(sheet.get_list_feed().entry):
+            # Skip the first row of units
+            if rownum == 0:
                 continue
-            vals.append(d[key])
-            cols.append(translate.get(key, key))
+            d = entry.to_dict()
+            # Units row has n/a as the date, so skip it
+            if d.get('date') in ['mm/dd/yyyy', '', None]:
+                continue
+            cols = []
+            vals = []
+            for key in d.keys():
+                if key.startswith('gio'):
+                    continue
+                vals.append(d[key])
+                cols.append(translate.get(key, key))
 
-        sql = """
-            INSERT into %s(%s) VALUES (%s)
-            """ % (table, ",".join(cols), ','.join(["%s"]*len(cols)))
-        try:
-            pcursor.execute(sql, vals)
-        except Exception as exp:
-            print("harvest_management traceback")
-            print exp
-            for a, b in zip(cols, vals):
-                print("   |%s| -> |%s|" % (a, b))
-        added += 1
+            sql = """
+                INSERT into %s(%s) VALUES (%s)
+                """ % (table, ",".join(cols), ','.join(["%s"]*len(cols)))
+            try:
+                pcursor.execute(sql, vals)
+            except Exception as exp:
+                print("harvest_management traceback")
+                print(exp)
+                for a, b in zip(cols, vals):
+                    print("   |%s| -> |%s|" % (a, b))
+            added += 1
 
-    for key in current.keys():
-        pcursor.execute("""DELETE from """+table+""" WHERE oid = %s""",
-                        (current[key],))
+        print(("harvest_management %16s added:%4s deleted:%4s"
+               ) % (sheetkey, added, deleted))
 
-    print(("harvest_management %16s rows:%4s dups:%4s add:%4s del:%4s"
-           ) % (sheetkey, entries, dups, added, len(current)))
+    pcursor.close()
+    pgconn.commit()
+    pgconn.close()
 
-pcursor.close()
-pgconn.commit()
-pgconn.close()
+
+if __name__ == '__main__':
+    main()
