45f3bd9df1916c657c82b5cf9c670633ddbd279a
akrherz <akrherz@iastate.edu>
2017-09-07 08:16:04 -0500

    address website issues found in #28

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,10 +1,11 @@
 <?php 
-include_once "../include/myview.php";
-$t = new MyView();
-$t->title = "ISU Data Team!";
-$t->content = <<<EOF
-<img src="images/dt-160307.jpg" class="img img-responsive">
-EOF;
-$t->render('single.phtml');
+Header("Location: /cscap/");
 
+//include_once "../include/myview.php";
+//$t = new MyView();
+//$t->title = "ISU Data Team!";
+//$t->content = <<<EOF
+//<img src="images/dt-160307.jpg" class="img img-responsive">
+//EOF;
+//$t->render('single.phtml');
 ?>
