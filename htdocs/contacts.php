ac949d353bd20caed87fa4a74aba4c1ff96f72ea
Giorgi Chighladze <giorgichi@gmail.com>
2017-09-14 23:09:38 -0500

    change format (#59)
    Changed paragraph style to match the format of the original text

--- htdocs/contacts.php
+++ htdocs/contacts.php
@@ -12,22 +12,22 @@ research and metadata collected, and protocols should be directed to
 The team members listed below will answer your questions. We will determine if contacting the individual 
 research site faculty and staff members are necessary.</p>
 
-<p>Lori Abendroth
+<pre>Lori Abendroth
 Sustainable Corn Project Manager, PI
 Agronomist
-<a href="mailto:labend@iastate.edu">labend@iastate.edu</a></p>
+<a href="mailto:labend@iastate.edu">labend@iastate.edu</a></pre>
 
-<p>Daryl Herzmann
+<pre>Daryl Herzmann
 Systems Administrator and Analyst
 Agricultural Meteorologist
-<a href="mailto:akrherz@iastate.edu">akrherz@iastsate.edu</a></p>
+<a href="mailto:akrherz@iastate.edu">akrherz@iastsate.edu</a></pre>
 
-<p>Giorgi Chighladze
+<pre>Giorgi Chighladze
 Data Manager and Analyst
 Agricultural Engineer
 <a href="mailto:gio@iastate.edu">gio@iastate.edu</a></p>
 
-<p><img src="images/dt-160307.jpg" class="img img-responsive"></p>
+<p><img src="images/dt-160307.jpg" class="img img-responsive"></pre>
 
 EOF;
 $t->render('single.phtml');
