81ccebaaa08a0379bb4e2f4e8d1228f8eace66ad
akrherz <akrherz@iastate.edu>
2017-09-18 09:59:10 -0500

    initial swing at plotting water quality

---
+++ htdocs/cscap/plot_waterquality.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+"""Plot!"""
+import sys
+import cStringIO
+import cgi
+import os
+import pandas as pd
+from pandas.io.sql import read_sql
+import psycopg2
+import numpy as np
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt  # NOPEP8
+
+VARDICT = {'WAT2': {'title': 'Nitrogen Concentration', 'units': 'mg N / L'},
+           'WAT9': {'title': 'Reactive Phosphorus Concentration',
+                    'units': 'ug P / L'}}
+
+
+def send_error(viewopt, msg):
+    """" """
+    if viewopt == 'js':
+        sys.stdout.write("Content-type: application/javascript\n\n")
+        sys.stdout.write("alert('No data found, sorry');")
+        sys.exit()
+    fig, ax = plt.subplots(1, 1)
+    ax.text(0.5, 0.5, msg, transform=ax.transAxes, ha='center')
+    sys.stdout.write("Content-type: image/png\n\n")
+    ram = cStringIO.StringIO()
+    fig.savefig(ram, format='png')
+    ram.seek(0)
+    sys.stdout.write(ram.read())
+    sys.exit()
+
+
+def make_plot(form):
+    """Make the plot"""
+    uniqueid = form.getfirst('site', 'ISUAG').split("::")[0]
+
+    pgconn = psycopg2.connect(database='sustainablecorn', host='iemdb',
+                              user='nobody')
+    viewopt = form.getfirst('view', 'plot')
+    varname = form.getfirst('varname', 'WAT2')
+    df = read_sql("""SELECT valid at time zone 'UTC' as v, plotid,
+    value
+    from waterquality_data WHERE uniqueid = %s
+    and varname = %s ORDER by valid ASC
+    """, pgconn, params=(uniqueid, varname))
+
+    if viewopt not in ['plot', 'js']:
+        df.rename(columns=dict(v='timestamp',
+                               value=varname
+                               ),
+                  inplace=True)
+        if viewopt == 'html':
+            sys.stdout.write("Content-type: text/html\n\n")
+            sys.stdout.write(df.to_html(index=False))
+            return
+        if viewopt == 'csv':
+            sys.stdout.write('Content-type: application/octet-stream\n')
+            sys.stdout.write(('Content-Disposition: attachment; '
+                              'filename=%s.csv\n\n'
+                              ) % (uniqueid, ))
+            sys.stdout.write(df.to_csv(index=False))
+            return
+        if viewopt == 'excel':
+            sys.stdout.write('Content-type: application/octet-stream\n')
+            sys.stdout.write(('Content-Disposition: attachment; '
+                              'filename=%s.xlsx\n\n'
+                              ) % (uniqueid, ))
+            writer = pd.ExcelWriter('/tmp/ss.xlsx',
+                                    options={'remove_timezone': True})
+            df.to_excel(writer, 'Data', index=False)
+            writer.save()
+            sys.stdout.write(open('/tmp/ss.xlsx', 'rb').read())
+            os.unlink('/tmp/ss.xlsx')
+            return
+
+    # Begin highcharts output
+    sys.stdout.write("Content-type: application/javascript\n\n")
+    title = ("Water Quality for Site: %s"
+             ) % (uniqueid, )
+    splots = []
+    plot_ids = df['plotid'].unique()
+    plot_ids.sort()
+    df['ticks'] = df['v'].astype(np.int64) // 10 ** 6
+    for plotid in plot_ids:
+        df2 = df[df['plotid'] == plotid]
+        splots.append(("""{type: 'scatter',
+            name: '"""+plotid+"""',
+            data: """ + str([[a, b] for a, b in zip(df2['ticks'].values,
+                                                    df2['value'].values)]) + """
+        }""").replace("None", "null").replace("nan", "null"))
+    series = ",".join(splots)
+    sys.stdout.write("""
+$("#hc").highcharts({
+    title: {text: '"""+title+"""'},
+    chart: {zoomType: 'x'},
+    yAxis: {title: {text: '""" + VARDICT[varname]["title"] + """'}
+    },
+    plotOptions: {line: {turboThreshold: 0}
+    },
+    xAxis: {
+        type: 'datetime'
+    },
+    tooltip: {
+        pointFormat: 'date: <b>{point.x:%b %e %Y, %H:%M}</b><br/>value: <b>{point.y}</b><br/>',
+        shared: true,
+        valueDecimals: 2,
+        valueSuffix: '""" + VARDICT[varname]["units"] + """'
+    },
+    series: ["""+series+"""]
+});
+    """)
+
+
+def main():
+    """Do Something"""
+    form = cgi.FieldStorage()
+    make_plot(form)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/cscap/waterquality/ingest_wq.py
@@ -0,0 +1,51 @@
+"""Water Quality ingest"""
+from __future__ import print_function
+import sys
+import datetime
+
+import pytz
+import psycopg2
+
+CENTRAL_TIME = ['ISUAG', 'GILMORE', 'SERF', 'CLAY_C', 'CLAY_R',
+                'MUDS2', 'MUDS3_OLD', 'MUDS4', 'SERF_SD', 'SERF_IA',
+                'STORY', 'UBWC']
+
+
+def gio_process(filename):
+    """This is a manually generated file by gio"""
+
+    pgconn = psycopg2.connect(database='sustainablecorn')
+    cursor = pgconn.cursor()
+    sql = """
+        INSERT into waterquality_data(uniqueid, plotid, valid,
+        sample_type, varname, value) VALUES (%s, %s, %s, %s, %s, %s)
+        """
+    for i, line in enumerate(open(filename)):
+        if i == 0:
+            continue
+        (uniqueid, plotid, date, localtime, sample_type, varname, value
+         ) = line.strip().split(",")
+        if localtime == '':
+            localtime = "00:00"
+        ts = datetime.datetime.strptime("%s %s" % (date, localtime),
+                                        '%Y-%m-%d %H:%M')
+        offset = 6 if uniqueid in CENTRAL_TIME else 5
+        ts = ts + datetime.timedelta(hours=offset)
+        ts = ts.replace(tzinfo=pytz.utc)
+        cursor.execute(sql, (uniqueid, plotid, ts, sample_type, varname,
+                             (float(value)
+                              if not value.startswith("<") else None)
+                             ))
+    cursor.close()
+    pgconn.commit()
+    pgconn.close()
+
+
+def main(argv):
+    """Go Main"""
+    fn = argv[1]
+    gio_process(fn)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
