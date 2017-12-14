f8cc72828e5500d536c9ad601dccd5c28021340b
akrherz <akrherz@iastate.edu>
2017-12-14 15:22:14 -0600

    Some TD tileflow harvesting work

--- scripts/td/harvest_tileflow_excel.py
+++ scripts/td/harvest_tileflow_excel.py
@@ -1,55 +1,57 @@
-import pandas as pd
-import psycopg2
+"""Process the Excel files that are manually downloaded from Google"""
+from __future__ import print_function
 import sys
-pgconn = psycopg2.connect(database='td')
-cursor = pgconn.cursor()
 
-dfs = pd.read_excel(sys.argv[1], sheetname=None, skiprows=[1],
-                    na_values=['Did not collect', 'Sensor malfunction'])
-uniqueid = sys.argv[2]
+import pandas as pd
+from pyiem.util import get_dbconn
 
-for sheetname in dfs.keys():
-    if len(sheetname) != 4:
-        print("Skipping sheet %s" % (sheetname, ))
-        continue
-    print("Processing sheet: %s" % (sheetname, ))
-    df = dfs[sheetname]
-    #for i, row in df.iterrows():
-    #    print row
-    #    _ = pd.to_datetime(row['Date'].strftime("%Y-%m-%d") +
-    #                       ' ' + row['Time'].strftime("%H:%M:%S"))
-    # print df
-    # print df['Time'].astype(str)
-    df['Date'] = pd.to_datetime(df['Date'], errors='coerse')
-    df['Time'] = pd.to_datetime(df['Time'], errors='coerse')
-    df['Date'] = df['Date'] + df['Time']
-    df.columns = ['Date', 'Time', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6']
-    cursor.execute("""
-        DELETE from tileflow_data where uniqueid = %s and
-        valid between %s and %s
-    """, (uniqueid, df['Date'].min(), df['Date'].max()))
-    deleted = cursor.rowcount
-    if deleted > 0:
-        print("Removed %s" % (deleted,))
 
-    inserts = 0
-    for plotid in ['S1', 'S2', 'S3', 'S4', 'S5', 'S6']:
-        ldf = df[['Date', plotid]]
-        for idx, row in ldf.iterrows():
-            if (row['Date'] == ' ' or row['Date'] is None or
-                    pd.isnull(row[plotid])):
-                continue
-            print row
+def main():
+    """Go Main Go"""
+    pgconn = get_dbconn('td')
+    cursor = pgconn.cursor()
+
+    dfs = pd.read_excel(sys.argv[1], sheetname=None, skiprows=[1],
+                        na_values=['Did not collect', 'Sensor malfunction'],
+                        converters={'Date': str, 'Time': str})
+    uniqueid = sys.argv[2]
+
+    for sheetname in dfs.keys():
+        if len(sheetname) != 4:
+            print("Skipping sheet %s" % (sheetname, ))
+            continue
+        print("Processing sheet: %s" % (sheetname, ))
+        df = dfs[sheetname]
+        #df['Date'] = df['Date'].astype('str') + ' ' + df['Time'].astype('str')
+        df['Date'] = pd.to_datetime(df['Date'])
+        df.columns = ['Date', 'Time', 'West', 'East']
+
+        for plotid in ['West', 'East']:
+            ldf = df[['Date', plotid]]
             cursor.execute("""
-            INSERT into tileflow_data
-            (uniqueid, plotid, valid, discharge_mm_qc, discharge_mm)
-            VALUES (%s, %s, %s, %s, %s)
-            """, (uniqueid, plotid, row['Date'],
-                  row.get(plotid), row.get(plotid)
-                  ))
-            inserts += 1
-        print("Inserted %s, Deleted %s entries for %s" % (inserts, deleted,
-                                                          uniqueid))
-cursor.close()
-pgconn.commit()
-pgconn.close()
+                DELETE from tileflow_data where uniqueid = %s and
+                valid between %s and %s and plotid = %s
+            """, (uniqueid, df['Date'].min(), df['Date'].max(), plotid))
+            deleted = cursor.rowcount
+            inserts = 0
+            for _idx, row in ldf.iterrows():
+                if (row['Date'] == ' ' or row['Date'] is None or
+                        pd.isnull(row[plotid])):
+                    continue
+                cursor.execute("""
+                INSERT into tileflow_data
+                (uniqueid, plotid, valid, discharge_mm_qc, discharge_mm)
+                VALUES (%s, %s, %s, %s, %s)
+                """, (uniqueid, plotid, row['Date'],
+                      row.get(plotid), row.get(plotid)
+                      ))
+                inserts += 1
+            print(("%s Inserted %s, Deleted %s entries for %s"
+                   ) % (plotid, inserts, deleted, uniqueid))
+    cursor.close()
+    pgconn.commit()
+    pgconn.close()
+
+
+if __name__ == '__main__':
+    main()
