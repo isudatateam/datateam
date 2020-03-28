fc671785013504103e6815ed2a41f2d899a49a66
akrherz <akrherz@iastate.edu>
2020-03-28 06:09:39 -0500

    add action columns to database

--- database/upgrade/td/13.sql
+++ database/upgrade/td/13.sql
@@ -3,3 +3,5 @@ ALTER TABLE plant_harvest add action text;
 ALTER TABLE plant_harvest add plotid text;
 ALTER TABLE soil_fert add plotid text;
 ALTER TABLE soil_fert add action text;
+ALTER TABLE residue_mngt add action text;
+ALTER TABLE irrigation add action text;
