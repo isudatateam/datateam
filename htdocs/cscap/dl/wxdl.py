d27e943e5745a5e35f9347e9823e451366af820f
akrherz <akrherz@iastate.edu>
2021-06-23 06:20:55 -0500

    prevent filename from getting too long

--- htdocs/cscap/dl/wxdl.py
+++ htdocs/cscap/dl/wxdl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 """Download weather data, please"""
-from __future__ import print_function
 import os
 import cgi
 import datetime
@@ -10,35 +9,39 @@ from pandas.io.sql import read_sql
 from pyiem.util import get_dbconn, ssw
 from pyiem.datatypes import distance, temperature
 
-VARDF = {'uniqueid': '',
-         'day': '',
-         'doy': 'Day Of Year',
-         'sknt': 'Wind Speed',
-         'drct': 'Wind Direction',
-         'high': 'High Temperature',
-         'highc': 'High Temperature',
-         'low': 'Low Temperature',
-         'lowc': 'Low Temperature',
-         'srad_mj': 'Solar Radiation',
-         'precip': 'Precipitation',
-         'precipmm': 'Precipitation'}
-UVARDF = {'uniqueid': '',
-          'day': '',
-          'doy': '',
-          'sknt': 'knots',
-          'drct': 'degrees N',
-          'high': 'F',
-          'highc': 'C',
-          'low': 'F',
-          'lowc': 'C',
-          'srad_mj': 'MJ/day',
-          'precip': 'inch',
-          'precipmm': 'mm'}
+VARDF = {
+    "uniqueid": "",
+    "day": "",
+    "doy": "Day Of Year",
+    "sknt": "Wind Speed",
+    "drct": "Wind Direction",
+    "high": "High Temperature",
+    "highc": "High Temperature",
+    "low": "Low Temperature",
+    "lowc": "Low Temperature",
+    "srad_mj": "Solar Radiation",
+    "precip": "Precipitation",
+    "precipmm": "Precipitation",
+}
+UVARDF = {
+    "uniqueid": "",
+    "day": "",
+    "doy": "",
+    "sknt": "knots",
+    "drct": "degrees N",
+    "high": "F",
+    "highc": "C",
+    "low": "F",
+    "lowc": "C",
+    "srad_mj": "MJ/day",
+    "precip": "inch",
+    "precipmm": "mm",
+}
 
 
 def sane_date(year, month, day):
-    """ Attempt to account for usage of days outside of the bounds for
-    a given month """
+    """Attempt to account for usage of days outside of the bounds for
+    a given month"""
     # Calculate the last date of the given month
     nextmonth = datetime.date(year, month, 1) + datetime.timedelta(days=35)
     lastday = nextmonth.replace(day=1) - datetime.timedelta(days=1)
@@ -46,14 +49,14 @@ def sane_date(year, month, day):
 
 
 def get_cgi_dates(form):
-    """ Figure out which dates are requested via the form, we shall attempt
-    to account for invalid dates provided! """
-    y1 = int(form.getfirst('year1'))
-    m1 = int(form.getfirst('month1'))
-    d1 = int(form.getfirst('day1'))
-    y2 = int(form.getfirst('year2'))
-    m2 = int(form.getfirst('month2'))
-    d2 = int(form.getfirst('day2'))
+    """Figure out which dates are requested via the form, we shall attempt
+    to account for invalid dates provided!"""
+    y1 = int(form.getfirst("year1"))
+    m1 = int(form.getfirst("month1"))
+    d1 = int(form.getfirst("day1"))
+    y2 = int(form.getfirst("year2"))
+    m2 = int(form.getfirst("month2"))
+    d2 = int(form.getfirst("day2"))
 
     ets = sane_date(y2, m2, d2)
     archive_end = datetime.date.today() - datetime.timedelta(days=1)
@@ -65,47 +68,53 @@ def get_cgi_dates(form):
 
 def do_work(form):
     """do great things"""
-    pgconn = get_dbconn('sustainablecorn')
-    stations = form.getlist('stations')
+    pgconn = get_dbconn("sustainablecorn")
+    stations = form.getlist("stations")
     if not stations:
         stations.append("XXX")
     sts, ets = get_cgi_dates(form)
-    df = read_sql("""
+    df = read_sql(
+        """
     SELECT station as uniqueid, valid as day, extract(doy from valid) as doy,
     high, low, precip, sknt,
     srad_mj, drct from weather_data_daily WHERE station in %s
     and valid >= %s and valid <= %s
     ORDER by station ASC, valid ASC
-    """, pgconn, params=(tuple(stations), sts, ets), index_col=None)
-    df['highc'] = temperature(df['high'].values, 'F').value('C')
-    df['lowc'] = temperature(df['low'].values, 'F').value('C')
-    df['precipmm'] = distance(df['precip'].values, 'IN').value('MM')
+    """,
+        pgconn,
+        params=(tuple(stations), sts, ets),
+        index_col=None,
+    )
+    df["highc"] = temperature(df["high"].values, "F").value("C")
+    df["lowc"] = temperature(df["low"].values, "F").value("C")
+    df["precipmm"] = distance(df["precip"].values, "IN").value("MM")
 
     metarows = [{}, {}]
     cols = df.columns
     for i, colname in enumerate(cols):
         if i == 0:
-            metarows[0][colname] = 'description'
-            metarows[1][colname] = 'units'
+            metarows[0][colname] = "description"
+            metarows[1][colname] = "units"
             continue
-        metarows[0][colname] = VARDF.get(colname, '')
-        metarows[1][colname] = UVARDF.get(colname, '')
+        metarows[0][colname] = VARDF.get(colname, "")
+        metarows[1][colname] = UVARDF.get(colname, "")
     df = pd.concat([pd.DataFrame(metarows), df], ignore_index=True)
     # re-establish the correct column sorting
     df = df.reindex(cols, axis=1)
 
-    writer = pd.ExcelWriter("/tmp/ss.xlsx", engine='xlsxwriter')
-    df.to_excel(writer, 'Daily Weather', index=False)
-    worksheet = writer.sheets['Daily Weather']
+    writer = pd.ExcelWriter("/tmp/ss.xlsx", engine="xlsxwriter")
+    df.to_excel(writer, "Daily Weather", index=False)
+    worksheet = writer.sheets["Daily Weather"]
     worksheet.freeze_panes(3, 0)
     writer.close()
 
     fn = ",".join(stations)
+    if len(stations) > 3:
+        fn = "cscap"
     ssw("Content-type: application/vnd.ms-excel\n")
-    ssw(("Content-Disposition: attachment;Filename=wx_%s.xls\n\n"
-         ) % (fn, ))
-    ssw(open('/tmp/ss.xlsx', 'rb').read())
-    os.unlink('/tmp/ss.xlsx')
+    ssw(f"Content-Disposition: attachment;Filename=wx_{fn}.xls\n\n")
+    ssw(open("/tmp/ss.xlsx", "rb").read())
+    os.unlink("/tmp/ss.xlsx")
 
 
 def main():
@@ -114,5 +123,5 @@ def main():
     do_work(form)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
