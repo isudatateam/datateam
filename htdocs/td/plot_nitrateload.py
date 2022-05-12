e6b375daae97b6b6c1d545d80177a6950e38d47e
akrherz <akrherz@iastate.edu>
2022-05-12 07:58:02 -0500

    fix some things with TD plotnitrate tool #458

--- htdocs/td/plot_nitrateload.py
+++ htdocs/td/plot_nitrateload.py
@@ -4,10 +4,9 @@ import sys
 import datetime
 
 import pandas as pd
-from pandas.io.sql import read_sql
 import numpy as np
 from paste.request import parse_formvars
-from pyiem.util import get_dbconn
+from pyiem.util import get_sqlalchemy_conn
 
 sys.path.append("/opt/datateam/htdocs/td")
 from common import CODES, getColor, send_error, COPYWRITE
@@ -48,7 +47,6 @@ BYCOL = {
 
 def make_plot(form, start_response):
     """Make the plot"""
-    pgconn = get_dbconn("td")
     siteid = form.get("site")
 
     sts = datetime.datetime.strptime(
@@ -59,54 +57,51 @@ def make_plot(form, start_response):
     ets = sts + datetime.timedelta(days=days)
     by = form.get("by", "daily")
     if ungroup == 0:
-        df = read_sql(
-            "WITH data as ("
-            f"SELECT date_trunc('{BYCOL[by]}', date)::date as v, "
-            "coalesce(plotid, location) as agg, "
-            "dwm_treatment as datum, "
-            "sum(nitrate_n_load) as nitrate_n_load "
-            "from tile_flow_and_n_loads_data WHERE "
-            "siteid = %s and date between %s and %s "
-            "and nitrate_n_load is not null and "
-            "dwm_treatment is not null and "
-            "dwm_treatment != 'Saturated Buffer' GROUP by v, agg, datum "
-            ") "
-            "SELECT v, datum, avg(nitrate_n_load) as nitrate_n_load "
-            "from data GROUP by v, datum "
-            "ORDER by v ASC",
-            pgconn,
-            params=(siteid, sts.date(), ets.date()),
-        )
+        with get_sqlalchemy_conn("td") as conn:
+            df = pd.read_sql(
+                """WITH data as (
+                SELECT date_trunc(%s, date)::date as v,
+                coalesce(plotid, location) as agg,
+                dwm_treatment as datum,
+                sum(nitrate_n_load) as nitrate_n_load
+                from tile_flow_and_n_loads_data WHERE
+                siteid = %s and date between %s and %s
+                and nitrate_n_load is not null and
+                dwm_treatment is not null GROUP by v, agg, datum
+                )
+                SELECT v, datum, avg(nitrate_n_load) as nitrate_n_load
+                from data GROUP by v, datum
+                ORDER by v ASC""",
+                conn,
+                params=(BYCOL[by], siteid, sts.date(), ets.date()),
+            )
 
     else:
-        df = read_sql(
-            f"SELECT date_trunc('{BYCOL[by]}', date)::date as v, "
-            "coalesce(plotid, location) as datum, "
-            "sum(nitrate_n_load_filled) as nitrate_n_load "
-            "from tile_flow_and_n_loads_data WHERE "
-            "siteid = %s and date between %s and %s "
-            "and nitrate_n_load is not null GROUP by v, datum "
-            "ORDER by v ASC",
-            pgconn,
-            params=(siteid, sts.date(), ets.date()),
-        )
+        with get_sqlalchemy_conn("td") as conn:
+            df = pd.read_sql(
+                """SELECT date_trunc(%s, date)::date as v,
+                coalesce(plotid, location) as datum,
+                sum(nitrate_n_load) as nitrate_n_load
+                from tile_flow_and_n_loads_data WHERE
+                siteid = %s and date between %s and %s
+                and nitrate_n_load is not null GROUP by v, datum
+                ORDER by v ASC""",
+                conn,
+                params=(BYCOL[by], siteid, sts.date(), ets.date()),
+            )
     if len(df.index) < 3:
         send_error(start_response, 1, "No / Not Enough Data Found, sorry!")
     linecol = "datum"
 
     # Begin highcharts output
     start_response("200 OK", [("Content-type", "application/javascript")])
-    title = ("Nitrate Load for %s (%s to %s)") % (
-        siteid,
-        sts.strftime("%-d %b %Y"),
-        ets.strftime("%-d %b %Y"),
-    )
+    title = f"Nitrate Load for {siteid} ({sts:%-d %b %Y} to {ets:%-d %b %Y})"
     s = []
     plot_ids = df[linecol].unique()
     plot_ids.sort()
     if ungroup == 1:
         plot_ids = plot_ids[::-1]
-    df["ticks"] = pd.to_datetime(df["v"]).astype(np.int64) // 10 ** 6
+    df["ticks"] = pd.to_datetime(df["v"]).astype(np.int64) // 10**6
     seriestype = "line" if by == "daily" else "column"
     for i, plotid in enumerate(plot_ids):
         df2 = df[df[linecol] == plotid]
