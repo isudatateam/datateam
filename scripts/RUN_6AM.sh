d0e5a72e1abcdf1e1b2808e040d4c28dcdbfbeef
akrherz <akrherz@iastate.edu>
2022-12-06 09:06:17 -0600

    drop td job and remove comments

--- scripts/RUN_6AM.sh
+++ scripts/RUN_6AM.sh
@@ -1,42 +1,11 @@
 cd /opt/datateam/scripts/cscap
 
-#python harvest_management.py
-#python harvest_agronomic.py 2011
-#python harvest_agronomic.py 2012
-#python harvest_agronomic.py 2013
-#python harvest_agronomic.py 2014
-#python harvest_agronomic.py 2015
-
-#python harvest_soil_nitrate.py 2011
-#python harvest_soil_nitrate.py 2012
-#python harvest_soil_nitrate.py 2013
-#python harvest_soil_nitrate.py 2014
-#python harvest_soil_nitrate.py 2015
-
-#python harvest_soil_texture.py 2011
-#python harvest_soil_texture.py 2012
-#python harvest_soil_texture.py 2013
-#python harvest_soil_texture.py 2014
-#python harvest_soil_texture.py 2015
-
-#python harvest_soil_bd.py 2011
-#python harvest_soil_bd.py 2012
-#python harvest_soil_bd.py 2013
-#python harvest_soil_bd.py 2014
-#python harvest_soil_bd.py 2015
-
-#python harvest_soil_fertility.py
-
-python email_daily_changes.py td
 python email_daily_changes.py inrc
 python email_daily_changes.py nutrinet
 python email_daily_changes.py kb
-# python email_daily_changes.py ardn
 python email_daily_changes.py sepac
 python email_daily_changes.py ilsoil
 python email_daily_changes.py cig
-
-#python ss2database.py
 
 cd ../auth
 python drive2webaccess.py
