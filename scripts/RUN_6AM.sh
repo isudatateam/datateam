fc0e6476ff6407c991f1c8ef7614300aed36b36c
akrherz <akrherz@iastate.edu>
2023-01-18 08:32:22 -0600

    drop SEPAC spammer

--- scripts/RUN_6AM.sh
+++ scripts/RUN_6AM.sh
@@ -3,7 +3,6 @@ cd /opt/datateam/scripts/cscap
 python email_daily_changes.py inrc
 python email_daily_changes.py nutrinet
 python email_daily_changes.py kb
-python email_daily_changes.py sepac
 python email_daily_changes.py ilsoil
 python email_daily_changes.py cig
 
