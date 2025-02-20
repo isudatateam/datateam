94ee01aa4a75b626ab215b3265e117595f72ac0c
akrherz <akrherz@iastate.edu>
2025-02-20 10:10:46 -0600

    🎨 Make pip installable

--- src/isudatateam/__init__.py
+++ src/isudatateam/__init__.py
@@ -1,1 +1,13 @@
 """Make importable."""
+
+import os
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("isudatateam")
+    pkgdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    if not pkgdir.endswith("site-packages"):
+        __version__ += "-dev"
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "dev"
